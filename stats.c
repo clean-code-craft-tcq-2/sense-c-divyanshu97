7b2b2563c3517c8bffb99e116509380c1cd447a9
Divyanshu Shukla <reachdivyanshu@gmail.com>
2021-10-25 14:05:01 +0530

    Test Case added and metadata calculation implemented

--- stats.c
+++ stats.c
@@ -5,6 +5,29 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
     s.average = 0;
     s.min = 0;
     s.max = 0;
+    if(setlength<=0)
+    {
+    	s.average =Nan;
+    	s.max =Nan;
+    	s.min =Nan;
+    }
+    else
+    {
+    	for(i=0;i<setlength;i++)
+    	{
+    		total += numberset[setlength];
+    		if(s.max < numberset[setlength])
+    		{
+    			s.max = numberset[setlength];
+    		}
+    		else if(s.min > numberset[setlength])
+    		{
+    			s.min = numberset[setlength];
+    		}
+    	}
+    	s.average = total / setlength;
+    }
+    return s;
 }
 
 int emailAlertCallCount = 0;
